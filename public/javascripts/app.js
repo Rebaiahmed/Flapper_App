9227a844bf9bc343214c1dc9ef870b3d34132ace
ahmedbouhmid <ahmed.bouhmid94@gmail.com>
2016-06-03 11:56:12 +0100

    publish to github

---
+++ public/javascripts/app.js
@@ -0,0 +1,15 @@
+var app = angular.module('flapperNews',[]);
+
+
+/*
+define our first controller
+ */
+
+app.controller('MainCtrl',['$scope',function($scope){
+
+
+    //test $scope
+    $scope.test = "hello " ;
+
+}])
+
